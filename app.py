4d5c94617219f1f46c6f26977b87cb3a76a803a8
sam <aquaticcalf@proton.me>
2023-10-22 15:34:53 +0530

    meow

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, send_file
-import PyPDF2
+from PyPDF2 import PdfReader, PdfWriter
 import io 
-import os
 
 app = Flask(__name__)
 
@@ -16,12 +15,30 @@ def compress_pdf():
     
     file = request.files['file']
 
+    output_name = request.form['output_name'] + ".pdf"
+
     if file.filename == '':
         return "select a file first, u idiot >:("
 
     if file:
         pdf = file.read()
-        return "yayy, yes file there"
+        
+        reader = PdfReader(io.BytesIO(pdf))
+        writer = PdfWriter()
 
+        for page in reader.pages:
+            page.compress_content_streams()
+            writer.add_page(page)
+
+        # create BytesIO object -> store the compressed pdf
+        compressed_pdf = io.BytesIO()
+        writer.write(compressed_pdf)
+        compressed_pdf.seek(0)  # reset the stream position
+
+    return send_file(
+        compressed_pdf,
+        as_attachment = True,
+        download_name = output_name
+    )
 if __name__ == '__main__':
     app.run(debug=True)
