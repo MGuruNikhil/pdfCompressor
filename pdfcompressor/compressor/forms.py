d87191a94135b4d22d378b6942691216f3abe849
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2023-12-06 19:21:55 +0530

    non pdf upload bug fixed

--- pdfcompressor/compressor/forms.py
+++ pdfcompressor/compressor/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 class PDFUploadForm(forms.Form):
-    original_pdf = forms.FileField(label="Choose a PDF file")
+    original_pdf = forms.FileField(
+        label="Choose a PDF file",
+        validators=[FileExtensionValidator(allowed_extensions=['pdf'])]
+    )
     compressed_pdf_name = forms.CharField(label="Compressed PDF Name", required=False)
